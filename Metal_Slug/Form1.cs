7b6ba880d3d4b952553f61b822959ea208727083
Omar El banna <bannxa2005@gmail.com>
2025-05-23 21:44:41 +0300

    Add files via upload

---
+++ Metal_Slug/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metal_Slug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Location = new Point(this.ClientSize.Width/2 + this.Width, this.ClientSize.Height/2 + this.Height/2);
+        }
+    }
+}
